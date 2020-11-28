d25a895ff8dd6a374ab6104daf74426bcfa59b1e
DevanEunbeeLee <numerous.plutos@gmail.com>
2020-11-28 18:16:54 +0900

    completed

---
+++ fitbit.js
@@ -0,0 +1,15 @@
+const sleep = document.querySelector(".js-fitbit");
+
+const API_KEY = "";
+
+function getSleepLogs(startDate, endDate) {
+    fetch(
+        "https://api.fitbit.com/1.2/user/mdcrc01059@gmail.com/sleep/date/2020-11-01/2020-11-02.json"
+    )
+}
+
+function init(){
+
+}
+
+init();
